1cd98221a53e38eb3a467756ee1f964c631d7f1d
Harbi Martin <harbimartin@gmail.com>
2023-01-31 12:13:16 +0700

    Test Voip

--- main.js
+++ main.js
@@ -2,38 +2,45 @@ const socket = io();
     // Connect to the server
 socket.connect();
 
+var startRecord = document.getElementById('startRecord');
 // Get audio stream from microphone
-// navigator.mediaDevices.getUserMedia({ audio: true })
-//     .then(stream => {
-//     // Create an audio context
-//     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-//     const source = audioContext.createMediaStreamSource(stream);
-//     const processor = audioContext.createScriptProcessor(1024, 1, 1);
+form.addEventListener('startRecord', function (e) {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+            // Create an audio context
+            const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+            const source = audioContext.createMediaStreamSource(stream);
+            const processor = audioContext.createScriptProcessor(1024, 1, 1);
 
-//     // Connect the audio source to the script processor
-//     source.connect(processor);
-//     processor.connect(audioContext.destination);
+            // Connect the audio source to the script processor
+            source.connect(processor);
+            processor.connect(audioContext.destination);
 
-//     // Process audio data
-//     processor.onaudioprocess = event => {
-//         const data = event.inputBuffer.getChannelData(0);
-//         socket.emit("stream-audio", data);
-//     };
-//     })
-//     .catch(error => {
-//     console.error(error);
-//     });
+            // Process audio data
+            processor.onaudioprocess = event => {
+                const data = event.inputBuffer.getChannelData(0);
+                socket.emit("stream-audio", data);
+            };
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
 
+var startPlay = document.getElementById('startPlay');
 // // Play audio stream from server
-// socket.on("audio-stream", data => {
-//     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-//     const source = audioContext.createBufferSource();
-//     const buffer = audioContext.createBuffer(1, data.length, audioContext.sampleRate);
-//     buffer.getChannelData(0).set(data);
-//     source.buffer = buffer;
-//     source.connect(audioContext.destination);
-//     source.start();
-// });
+form.addEventListener('startPlay', function (e) {
+    e.preventDefault();
+    socket.on("audio-stream", data => {
+        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        const source = audioContext.createBufferSource();
+        const buffer = audioContext.createBuffer(1, data.length, audioContext.sampleRate);
+        buffer.getChannelData(0).set(data);
+        source.buffer = buffer;
+        source.connect(audioContext.destination);
+        source.start();
+    });
+});
 
 
 var messages = document.getElementById('messages');
@@ -43,8 +50,8 @@ var input = document.getElementById('input');
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     if (input.value) {
-    socket.emit('chat message', input.value);
-    input.value = '';
+        socket.emit('chat message', input.value);
+        input.value = '';
     }
 });
 
