8d7563a0886de3f05e88296bdeb233251887e806
Harbi Martin <harbimartin@gmail.com>
2023-01-31 14:06:57 +0700

    Test File Cache

--- app.js
+++ app.js
@@ -42,8 +42,8 @@ startPlay.addEventListener('click', function (e) {
     socket.on("audio-stream", data => {
         const audioContext = new (window.AudioContext || window.webkitAudioContext)();
         const source = audioContext.createBufferSource();
-        const buffer = audioContext.createBuffer(0, data.length, audioContext.sampleRate);
-        buffer.getChannelData(0).set(data);
+        const buffer = audioContext.createBuffer(1, data.length, audioContext.sampleRate);
+        buffer.getChannelData(1).set(data);
         source.buffer = buffer;
         source.connect(audioContext.destination);
         source.start();
