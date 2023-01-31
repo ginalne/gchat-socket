2dd50e596a73ce455539842c2c31c272c9133582
Harbi Martin <harbimartin@gmail.com>
2023-01-31 12:03:22 +0700

    VoIP Example

--- main.js
+++ main.js
@@ -3,37 +3,37 @@ const socket = io();
 socket.connect();
 
 // Get audio stream from microphone
-navigator.mediaDevices.getUserMedia({ audio: true })
-    .then(stream => {
-    // Create an audio context
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    const source = audioContext.createMediaStreamSource(stream);
-    const processor = audioContext.createScriptProcessor(1024, 1, 1);
+// navigator.mediaDevices.getUserMedia({ audio: true })
+//     .then(stream => {
+//     // Create an audio context
+//     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+//     const source = audioContext.createMediaStreamSource(stream);
+//     const processor = audioContext.createScriptProcessor(1024, 1, 1);
 
-    // Connect the audio source to the script processor
-    source.connect(processor);
-    processor.connect(audioContext.destination);
+//     // Connect the audio source to the script processor
+//     source.connect(processor);
+//     processor.connect(audioContext.destination);
 
-    // Process audio data
-    processor.onaudioprocess = event => {
-        const data = event.inputBuffer.getChannelData(0);
-        socket.emit("stream-audio", data);
-    };
-    })
-    .catch(error => {
-    console.error(error);
-    });
+//     // Process audio data
+//     processor.onaudioprocess = event => {
+//         const data = event.inputBuffer.getChannelData(0);
+//         socket.emit("stream-audio", data);
+//     };
+//     })
+//     .catch(error => {
+//     console.error(error);
+//     });
 
-// Play audio stream from server
-socket.on("audio-stream", data => {
-    const audioContext = new (window.AudioContext || window.webkitAudioContext)();
-    const source = audioContext.createBufferSource();
-    const buffer = audioContext.createBuffer(1, data.length, audioContext.sampleRate);
-    buffer.getChannelData(0).set(data);
-    source.buffer = buffer;
-    source.connect(audioContext.destination);
-    source.start();
-});
+// // Play audio stream from server
+// socket.on("audio-stream", data => {
+//     const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+//     const source = audioContext.createBufferSource();
+//     const buffer = audioContext.createBuffer(1, data.length, audioContext.sampleRate);
+//     buffer.getChannelData(0).set(data);
+//     source.buffer = buffer;
+//     source.connect(audioContext.destination);
+//     source.start();
+// });
 
 
 var messages = document.getElementById('messages');
