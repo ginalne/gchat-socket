5693d74c8151d10d682294d5244a577ee6db971d
Harbi Martin <harbimartin@gmail.com>
2023-01-31 12:04:24 +0700

    Test Voip

--- index.js
+++ index.js
@@ -11,20 +11,20 @@ io.on("connection", socket => {
   console.log("Client connected");
 
   // Listen for 'stream-audio' event from client
-  socket.on("stream-audio", data => {
-    // Emit 'audio-stream' event to all connected clients
-    socket.broadcast.emit("audio-stream", data);
-  });
+  // socket.on("stream-audio", data => {
+  //   // Emit 'audio-stream' event to all connected clients
+  //   socket.broadcast.emit("audio-stream", data);
+  // });
 
+  socket.on('chat message', msg => {
+    console.log("Chat EMIT : " + msg);
+    io.emit('chat message', msg);
+  });
   // Listen for client disconnection
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
   
-  socket.on('chat message', msg => {
-    console.log("Chat EMIT : " + msg);
-    io.emit('chat message', msg);
-  });
 });
 http.listen(port,'127.0.0.1', () => {
   console.log(`Socket.IO server running at http://localhost:${port}/`);
